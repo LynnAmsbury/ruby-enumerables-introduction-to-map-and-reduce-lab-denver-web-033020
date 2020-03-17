e558054d6b245ec64e7b31a32812a605a075833e
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-17 18:36:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,11 +49,29 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  total = starting_point
   i = 0
   while i < source_array.length do
-    total = total + source_array[i]
+    if (source_array[i] == true)
+      
+    end
     i += 1
   end
   total
 end
+
+def i_hear_a_sick_sound(passengers_sounds)
+  i = 0 # set up a i for the enumeration of the passengers collection
+  while i < passengers_sounds.length do # a loop for each passenger
+    # Stop enumerating and return true if any passenger is
+    # coughing or sneezing
+    if (passengers_sounds[i] == "coughing" || passengers_sounds[i] == "sneezing")
+      return true
+    end
+    i += 1
+  end
+  return false
+end
+ 
+i_hear_a_sick_sound(["coughing", "foo", "bar", "bin", "bat"]) #=> true
+i_hear_a_sick_sound(["sneezing", "foo", "bar", "bin", "bat"]) #=> true
+i_hear_a_sick_sound([            "foo", "bar", "bin", "bat"]) #=> false
